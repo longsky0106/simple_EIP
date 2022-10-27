05851d01ea76ecbdb8275108c60554b98f17b086
posi.shen <pct-mis@pct-max.com.tw>
2022-10-27 11:24:44 +0800

    加入State與路徑檢查當前頁面，以避免在別的頁面繼續後台傳輸

--- JS/ly_online_user.js
+++ JS/ly_online_user.js
@@ -26,22 +26,36 @@
 
 	$(function(){
 		setInterval(function(){
-		   $.post(root_path + "F_show_online_user.php", {
-				sys_type: "生產製造"
-			}, function(result){
-				$("#ly_bom").html(result);
-			});
-			$.post(root_path + "F_show_online_user.php", {
-				sys_type: "出口貿易"
-			}, function(result){
-				$("#LyTrade").html(result);
-			});
-			$.post(root_path + "F_show_online_user.php", {
-				sys_type: "會計財務"
-			}, function(result){
-				$("#LyAct").html(result);
-			});
-		}, 15 * 1000);
+			root_path1 = window.location.pathname;
+			root_path1 = root_path1.indexOf("php");
+			let searchParams = new URLSearchParams(window.location.search);
+			let url = searchParams.get('url');
+			if (typeof(url) === 'undefined') {
+				url = "url";
+			}
+			if(url=="show_online_user.php" || root_path1 > 0){
+				//alert("u");
+				$.post(root_path + "F_show_online_user.php", {
+					sys_type: "生產製造"
+				}, function(result){
+					$("#ly_bom").html(result);
+				});
+				$.post(root_path + "F_show_online_user.php", {
+					sys_type: "出口貿易"
+				}, function(result){
+					$("#LyTrade").html(result);
+				});
+				$.post(root_path + "F_show_online_user.php", {
+					sys_type: "會計財務"
+				}, function(result){
+					$("#LyAct").html(result);
+				});
+				
+			}
+
+			
+		   
+		}, 5 * 1000);
 	});
 
 	
