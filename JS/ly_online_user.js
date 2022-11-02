2ebf51162660350d821193bc2ea2e2236a444f0c
posi.shen <pct-mis@pct-max.com.tw>
2022-11-02 15:57:43 +0800

    整理與修正秒數

--- JS/ly_online_user.js
+++ JS/ly_online_user.js
@@ -27,38 +27,35 @@
 	// 防止重複執行
 	if (typeof(root_path1) === 'undefined') {
 		$(function(){
-		setInterval(function(){
-			root_path1 = window.location.pathname;
-			root_path1 = root_path1.indexOf("php");
-			let searchParams = new URLSearchParams(window.location.search);
-			let url = searchParams.get('url');
-			if (typeof(url) === 'undefined') {
-				url = "url";
-			}
-			if(url=="show_online_user.php" || root_path1 > 0){
-				//alert("u");
-				$.post(root_path + "F_show_online_user.php", {
-					sys_type: "生產製造"
-				}, function(result){
-					$("#ly_bom").html(result);
-				});
-				$.post(root_path + "F_show_online_user.php", {
-					sys_type: "出口貿易"
-				}, function(result){
-					$("#LyTrade").html(result);
-				});
-				$.post(root_path + "F_show_online_user.php", {
-					sys_type: "會計財務"
-				}, function(result){
-					$("#LyAct").html(result);
-				});
-				
-			}
-
-			
-		   
-		}, 5 * 1000);
-	});
+			setInterval(function(){
+				root_path1 = window.location.pathname;
+				root_path1 = root_path1.indexOf("php");
+				let searchParams = new URLSearchParams(window.location.search);
+				let url = searchParams.get('url');
+				if (typeof(url) === 'undefined') {
+					url = "url";
+				}
+				if(url=="show_online_user.php" || root_path1 > 0){
+					//alert("u");
+					$.post(root_path + "F_show_online_user.php", {
+						sys_type: "生產製造"
+					}, function(result){
+						$("#ly_bom").html(result);
+					});
+					$.post(root_path + "F_show_online_user.php", {
+						sys_type: "出口貿易"
+					}, function(result){
+						$("#LyTrade").html(result);
+					});
+					$.post(root_path + "F_show_online_user.php", {
+						sys_type: "會計財務"
+					}, function(result){
+						$("#LyAct").html(result);
+					});
+					
+				}
+			}, 15 * 1000);
+		});
 	}
 	
 
